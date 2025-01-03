78f8c98e754791c061a32bcc13f3a753bf6171cf
Johannes Cleve <johannescleve@me.com>
2025-01-03 18:27:00 +0100

    words

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,41 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { BehaviorSubject, fromEvent, Subscription } from 'rxjs';
+import { Word, words } from '../data';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
-export class AppComponent {
-  title = 'study-korean';
+export class AppComponent implements OnInit, OnDestroy {
+  private subscription: Subscription = new Subscription();
+
+  private words: Word[] = words
+  private index = 0;
+
+  public word$ = new BehaviorSubject<Word>(this.words[this.index]);
+
+
+  public ngOnInit() {
+    this.subscription.add(
+      fromEvent(document, 'click').subscribe(() => this.next())
+    );
+  }
+
+  public ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+
+  private next() {
+    if(this.index === this.words.length - 1) {
+      this.index = 0
+    } else {
+      this.index++
+    }
+
+    this.word$.next(this.words[this.index])
+  }
 }
---
+++ src/data/words.ts
@@ -0,0 +1,14 @@
+export interface Word { ko: string; en: string}
+
+export const words: Word[] = [
+  { en: 'hello', ko: '안녕하세요' },
+  { en: 'thank you', ko: '감사합니다' },
+  { en: 'yes', ko: '네' },
+  { en: 'no', ko: '아니요' },
+  { en: 'goodbye', ko: '안녕히 가세요' },
+  { en: 'please', ko: '주세요' },
+  { en: 'sorry', ko: '죄송합니다' },
+  { en: 'water', ko: '물' },
+  { en: 'food', ko: '음식' },
+  { en: 'friend', ko: '친구' }
+]
